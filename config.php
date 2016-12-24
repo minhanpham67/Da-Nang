ac5e05ec67855646f4f2958ff9d04f5b4ef94b9e
minhanpham67 <minhanpham67@outlook.com.vn>
2016-12-25 00:09:19 +0700

    Update config.php

--- config.php
+++ config.php
@@ -1,10 +1,10 @@
 <?php
 
-    define("APPID", "");
-    define("APPSECRET", "");
-    define("PAGEID", "");
-    define("BASEURL", "");
-    define("MESSAGE", "");
-    define("PORTAL", "");
+    define("APPID", "1280779291951536");
+    define("APPSECRET", "07dcfb3aa8350af46c305fd5be2a7da0");
+    define("PAGEID", "https://www.facebook.com/DaNangMobileCam/");
+    define("BASEURL", "https://developers.facebook.com/apps/1280779291951536/");
+    define("MESSAGE", "wwewewwew");
+    define("PORTAL", "https://www.google.com.vn/");
 
 ?>
